a1ed35c5b995d9a46081a9c05961292f6b6519cf
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-10-18 12:37:58 +0000

    get function

---
+++ get_function.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * get_function - gets the right function according to the specifier
+ * @c: character to find corresponding to the specifier
+ *
+ * Return: return a function pointer or NULL
+ */
+int(*get_function(char c))(va_list a, char *s, int *index)
+{
+	int i;
+	choose ph[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'i', print_integer},
+		{'d', print_integer},
+		{'\0', NULL}
+	};
+	for (i = 0; ph[i].c != '\0'; i++)
+	{
+		if (c == ph[i].c)
+		{
+			return (ph[i].f);
+		}
+	}
+	return (NULL);
+}
