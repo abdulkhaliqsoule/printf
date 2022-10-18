65a67d6df8497ce3985bb496ab96ef24cf0825a8
kuksabb <abbanekua@gmail.com>
2022-10-18 04:31:48 -0700

    Added comments to main.h

--- main.h
+++ main.h
@@ -14,6 +14,7 @@ typedef struct a
 	    char c;
 	    int (*f)(va_list list, char *s, int *index);
 } choose;
+
 /**
  * _putchar - writes the character c to stdout
  * @c: character to print
@@ -22,6 +23,7 @@ typedef struct a
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
+
 /**
  * _printf - produces output according to a format.
  * @format: is a character string
@@ -29,12 +31,44 @@ int _putchar(char c);
  * Return: the number of characters printed.
  */
 int _printf(const char *format, ...);
-int _putchar(char c);
+
 void set_buffer(char *s, char x, int *index);
 int (*get_function(char c))(va_list a, char *s, int *index);
+
+/**
+ * print_char - print characters
+ * @a: element of va_list type
+ * @s: buffer
+ * @index: position on buffer
+ * Return: number of "characters" of element called
+*/
 int print_char(va_list a, char *s, int *index);
+
+/**
+ * print_string - print strings
+ * @a: element of va_list type
+ * @s: buffer
+ * @index: position on buffer
+ * Return: number of characters of element called
+*/
 int print_string(va_list a, char *s, int *index);
+
+/**
+ * print_percent - print character %
+ * @a: element of va_list type
+ * @s: buffer
+ * @index: position on buffer
+ * Return: number of "characters" of element called
+*/
 int print_percent(va_list a, char *s, int *index);
+
+/**
+ * print_integer - print numbers in base 10 (integers)
+ * @a: element of va_list type
+ * @s: buffer
+ * @index: position on buffer
+ * Return: number of "characters" of element called
+*/
 int print_integer(va_list a, char *s, int *index);
 
 #endif
